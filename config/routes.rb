9967e40cf2e026b76d79d447dfe52b002e30be86
Elmer Ibayan <eibayan@gmail.com>
2016-09-22 13:12:45 +1000

    Modify route and home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :contracts 
 
-  get 'pages/home'
+  //get 'pages/home'
+  root 'pages#home'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
