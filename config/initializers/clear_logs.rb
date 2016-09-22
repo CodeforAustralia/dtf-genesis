fdc3e68e2a34f1c62a674c534a777b7d12c97ec7
Elmer Ibayan <eibayan@gmail.com>
2016-09-22 11:22:22 +1000

    Added clear development log file

---
+++ config/initializers/clear_logs.rb
@@ -0,0 +1,10 @@
+#http://rubyquicktips.com/post/17594543524/clear-your-development-logs-automatically-when
+if Rails.env.development?
+  MAX_LOG_SIZE = 2.megabytes
+  
+  logs = File.join(Rails.root, 'log', '*.log')
+  if Dir[logs].any? {|log| File.size?(log).to_i > MAX_LOG_SIZE }
+    $stdout.puts "Runing rake log:clear"
+    `rake log:clear`
+  end 
+end
