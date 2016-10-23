4aff5311c73b5fddd9f2f7cb1bf03ccce9b2eabe
PuZZleDucK <puzzleduck@gmail.com>
2016-10-23 19:55:53 +1100

    reset to normal schedule for deployment

--- lib/external_scripts/server_keepalive.rb
+++ lib/external_scripts/server_keepalive.rb
@@ -4,16 +4,16 @@ require 'capybara/poltergeist'
 
 scheduler = Rufus::Scheduler.singleton
 
-wake_job = scheduler.every '1h', :first_at => Time.parse("3:55:00 pm") do
-#wake_job = scheduler.every '1d', :first_at => Time.parse("11:25:00 pm") do
+#wake_job = scheduler.every '1h', :first_at => Time.now + 15 do # DEBUG
+wake_job = scheduler.every '1d', :first_at => Time.parse("11:25:00 pm") do
     puts "Log || SCRAPER KEEPALIVE || #{Time.now.strftime("%I:%M:%S %P")}"
     Capybara.javascript_driver = :poltergeist
     session = Capybara::Session.new(:poltergeist)
     session.driver.browser.js_errors = false
     2.times do
       puts "visit"
-      session.visit "http://dtf-genesis.herokuapp.com/"
       session.visit "http://cfa-dtf.herokuapp.com"
+      session.visit "http://dtf-genesis.herokuapp.com/"
       sleep 5 * 60
     end
     session.driver.quit
