d14545768d6c903f97ae882f6529a7c31da25192
PuZZleDucK <puzzleduck@gmail.com>
2016-10-25 17:06:10 +1100

    add unspsc model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161021024507) do
+ActiveRecord::Schema.define(version: 20161025060203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,13 @@ ActiveRecord::Schema.define(version: 20161021024507) do
     t.string   "parent_category"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "unspscs", force: :cascade do |t|
+    t.integer  "unspsc_code"
+    t.string   "unspsc_name"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
