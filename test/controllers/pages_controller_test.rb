f055433ad2de3df5af0576cd0e5653e3eb6e9afc
PuZZleDucK <puzzleduck@gmail.com>
2016-10-27 00:01:53 +1100

    back on the testing rails

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -3,14 +3,14 @@ require 'capybara/rails'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
 
-  # test "should get home" do
-    # visit(pages_home_url)
-    # screenshot_and_save_page
+  test "should get home" do
+    visit(pages_home_url)
+    screenshot_and_save_page
     # Percy::Capybara.snapshot(page, name: 'home-basic')
-    #
-    # assert page.has_content?("working very hard")
-    # assert_match root_path, current_path
-    # assert_includes 200...300, page.status_code
-  # end
+
+    assert page.has_content?("working very hard")
+    assert_match root_path, current_path
+    assert_includes 200...300, page.status_code
+  end
 
 end
--- test/models/contract_test.rb
+++ test/models/contract_test.rb
@@ -9,48 +9,49 @@ class ContractTest < ActiveSupport::TestCase
                                 start_date: Date.parse("11/10/1979"),
                                 end_date: Date.parse("16/10/1979"),
                                 total_value: 1000000 )
+    # @contract = Contract.create()
   end
 
-  # test "sanity check contract" do
-    # assert @contract.valid?
-  # end
+  test "sanity check contract" do
+    assert @contract.valid?
+  end
 
-  # test "internal contract number should be unique" do # even if department # is not
-    # @clone = @contract.dup
-    # @contract.contract_number = "something new"
-    # @contract.save
+  test "internal contract number should be unique" do # even if department # is not
+    @clone = @contract.dup
+    @contract.contract_number = "something new"
+    @contract.save
     # TODO: contract # validation
-#    puts "C:#{@contract.id}"
-#    puts "c:#{@clone.id}"
-#    assert_not @clone.valid?
-  # end
+  #  puts "C:#{@contract.department_index}"
+  #  puts "c:#{@clone.department_index}"
+  #  assert_not @clone.valid?
+  end
 
   # test "department should not be blank" do # even if department # is not
     # TODO: add department
   # end
 
-  # test "contract_number should not be blank" do # even if department # is not
-    # @contract.contract_number = "         "
-#    assert_not @contract.valid?
-  # end
+  test "contract_number should not be blank" do # even if department # is not
+    @contract.contract_number = "         "
+   assert_not @contract.valid?
+  end
 
 
-  # test "title should not be blank" do # even if department # is not
-    # @contract.title = "         "
-#    assert_not @contract.valid?
-  # end
+  test "title should not be blank" do # even if department # is not
+    @contract.title = "         "
+   assert_not @contract.valid?
+  end
 
 
-  # test "start_date should not be nil" do # even if department # is not
-    # @contract.start_date = nil
-#    assert_not @contract.valid?
-  # end
+  test "start_date should not be nil" do # even if department # is not
+    @contract.start_date = nil
+   assert_not @contract.valid?
+  end
 
 
-  # test "total_value should not be nil" do # even if department # is not
-    # @contract.total_value = nil
-#    assert_not @contract.valid?
-  # end
+  test "total_value should not be nil" do # even if department # is not
+    @contract.total_value = nil
+   assert_not @contract.valid?
+  end
 
 
 
