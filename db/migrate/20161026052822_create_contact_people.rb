e6da9ba9028a44d70a56d6eccc91db4429a5a14a
PuZZleDucK <puzzleduck@gmail.com>
2016-10-27 00:01:02 +1100

    create contact data model

--- db/migrate/20161026052822_create_contact_people.rb
+++ db/migrate/20161026052822_create_contact_people.rb
@@ -1,10 +1,6 @@
 class CreateContactPeople < ActiveRecord::Migration[5.0]
   def change
     create_table :contact_people do |t|
-      t.string :name
-      t.string :phone
-      t.string :fax
-      t.string :email
 
       t.timestamps
     end
