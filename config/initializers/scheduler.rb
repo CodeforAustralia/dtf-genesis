5ae7c362da43d872dbfd432b83b977eac73f0066
PuZZleDucK <puzzleduck@gmail.com>
2016-10-30 21:45:52 +1100

    storing more scraped data

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -15,12 +15,71 @@ def find_between(text, pre_string, post_string)
   end
 end
 
+def lookup_department_id(department_text)
+  if department_text.include?("CenITex")
+    5154
+  elsif department_text.include?("Department of Economic Development, Jobs, Transport and Resources")
+    43087
+  elsif department_text.include?("Department of Education and Training")
+    42999
+  elsif department_text.include?("Department of Environment and Primary Industries")
+    43
+  elsif department_text.include?("Department of Environment, Land, Water and Planning")
+    42924
+  elsif department_text.include?("Department of Health & Human Services (DHHS)")
+    42963
+  elsif department_text.include?("Department of Health and Human Services - Infrastructure Planning and Delivery")
+    43010
+  elsif department_text.include?("Department of Justice & Regulation")
+    43004
+  elsif department_text.include?("Department of Premier and Cabinet")
+    10
+  elsif department_text.include?("Independent Broad-based Anti-corruption Commission")
+    42425
+  elsif department_text.include?("Infrastructure Victoria")
+    52488
+  elsif department_text.include?("Major Projects Victoria")
+    20135
+  elsif department_text.include?("Metropolitan Fire and Emergency Services Board")
+    3161
+  elsif department_text.include?("State Revenue Office")
+    15
+  elsif department_text.include?("Victoria Police")
+    39
+  elsif department_text.include?("Victorian Auditor General's Office")
+    5979
+  elsif department_text.include?("Victorian Commission for Gambling and Liquor Regulation")
+    33602
+  elsif department_text.include?("Whole of Victorian Government")
+    18641
+  elsif department_text.include?("WoVG Land Sales")
+    18669
+  end
+end
+
+def lookup_contract_type(text)
+  0
+end
+
+def lookup_value_type(text)
+  0
+end
+
+def lookup_contract_status(text)
+  0
+end
+
+def lookup_contract_unspsc(text)
+  30000000
+end
+
 def extract_contract_data(text, contract_index)
   gov_entity = find_between(text, "Public Body:", "Contract Number:")
   gov_entity_contract_numb = find_between(text, "Contract Number:","Title:")
   contract_title = find_between(text, "Title:","Type of Contract:")
   contract_type = find_between(text, "Type of Contract:","Total Value of the Contract:")
-  contract_value = (find_between(text, "Total Value of the Contract:","Start Date:").gsub(",","").gsub("$","").to_f).to_i
+  value_string = find_between(text, "Total Value of the Contract:","Start Date:")
+  contract_value = (value_string.gsub(",","").gsub("$","").to_f).to_i
   begin
     contract_start = Date.parse(find_between(text, "Start Date:","Expiry Date:"))
   rescue
@@ -36,13 +95,16 @@ def extract_contract_data(text, contract_index)
   contract_details = find_between(text, "Description", "Agency Contact Details")
   { gov_entity: gov_entity,
     gov_entity_contract_numb: gov_entity_contract_numb,
+    gov_entity_id_numb: lookup_department_id(gov_entity),
     contract_title: contract_title,
-    contract_type: contract_type,
+    contract_type: lookup_contract_type(contract_type),
     contract_value: contract_value,
+    value_type: lookup_value_type(value_string),
     contract_start: contract_start,
     contract_end: contract_end,
-    contract_status: contract_status,
-    contract_unspsc: contract_unspsc,
+    contract_status: lookup_contract_status(contract_status),
+    contract_unspsc: lookup_contract_unspsc(contract_unspsc),
+    contract_details: contract_details,
     vt_contract_index: contract_index
   }
 end
@@ -57,12 +119,31 @@ def store_or_skip(contract_data)
   elsif Contract.find_by(contract_number: contract_data[:gov_entity_contract_numb])
     nil
   else
-    Contract.create!(contract_number: contract_data[:gov_entity_contract_numb],
-                     status: contract_data[:contract_status],
-                     title: contract_data[:contract_title],
-                     start_date: contract_data[:contract_start],
-                     end_date: contract_data[:contract_end],
-                     total_value: contract_data[:contract_value] )
+    # Contract.create!(contract_number: contract_data[:gov_entity_contract_numb],
+    #                  status: contract_data[:contract_status],
+    #                  title: contract_data[:contract_title],
+    #                  start_date: contract_data[:contract_start],
+    #                  end_date: contract_data[:contract_end],
+    #                  total_value: contract_data[:contract_value] )
+
+    Contract.create!({
+      contract_number: contract_data[:gov_entity_contract_numb],
+      status: contract_data[:contract_status],
+      title: contract_data[:contract_title],
+      start_date: contract_data[:contract_start],
+      end_date: contract_data[:contract_end],
+      total_value: contract_data[:contract_value],
+      department_index: contract_data[:gov_entity_id_numb],
+      contract_type_index: contract_data[:contract_type],
+      value_type_index: contract_data[:value_type],
+      status_index: contract_data[:contract_status],
+      unspc_code: contract_data[:contract_unspsc],
+      contract_description: contract_data[:contract_details],
+      department_id: Faker::Number.between(0, 19),
+      supplier_id: 0,
+      contact_id: 0,
+      address: "",
+      })
   end
 end
 
@@ -136,7 +217,7 @@ daily_scrape = scheduler.every '1d', :first_at => Time.parse("11:55:00 pm") do #
   contract_indexes_to_scrape = scrape_for_references("https://www.tenders.vic.gov.au/tenders/contract/list.do?action=contract-view")
   contract_session = prepare_session()
   Capybara.reset_sessions!
-  contract_indexes_to_scrape.each do |contract_index|
+  contract_indexes_to_scrape.to_set.each do |contract_index|
     contract_session.visit "http://www.tenders.vic.gov.au/tenders/contract/view.do?id=#{contract_index}"
     contract_data = extract_contract_data(contract_session.text, contract_index)
     store_or_skip(contract_data)
