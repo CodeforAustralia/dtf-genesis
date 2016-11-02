df1ce21094c3c086a074abd281802098e65ba207
PuZZleDucK <puzzleduck@gmail.com>
2016-11-02 23:00:33 +1100

    smoke testing contract data extraction

--- test/integration/scraper_test.rb
+++ test/integration/scraper_test.rb
@@ -45,4 +45,45 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     assert_equal 0, lookup_contract_unspsc("")
   end
 
+  test "extract contract succeeds" do
+    contract_object = extract_contract_data("Contract - HYCR037 Contract Details   Public Body: 	Department of Health and Human Services - Infrastructure Planning and Delivery Contract Number: 	HYCR037 Title: 	Royal Victorian Eye and Ear Hospital Redevelopment - Main Works Basement and Civil Works Type of Contract: 	Construction Contracts Total Value of the Contract: 	$410,000.00 (Fixed Price) Start Date: 	4 Apr, 2016 Expiry Date: 	5 Apr, 2018 Status: 	Current UNSPSC : 	Building and Construction and Maintenance Services - (100%) Description 	  Royal Victorian Eye and Ear Hospital Redevelopment (RVEEH)  Main Works Basement and Civil Works Agency Contact Details Contact Person: 	Rachel Devine Contact for factual information purposes. Explanation of contracts or interpretation of specific clauses is not provided. Contact Number: 	PHONE: (03) 90967295 Email Address: 	Rachel.Devine@dhhs.vic.gov.au Supplier Information Supplier Name: 	Ace Civil Services Pty Ltd ABN: 	16131093466 ACN: 	131093466 DUNS #: 	 Address: 	18 Brisbane Street Suburb: 	Eltham State: 	VIC Postcode: 	3095 Email Address: ", 0) # text, contract_index
+    expected_response = {
+      gov_entity: "Department of Health and Human Services - Infrastructure Planning and Delivery",
+      gov_entity_contract_numb: "HYCR037", # 43010
+      gov_entity_id_numb: 43010,
+      contract_title: "Royal Victorian Eye and Ear Hospital Redevelopment - Main Works Basement and Civil Works",
+      contract_type: 0,
+      contract_value: 410000,
+      value_type: 0,
+      contract_start: Date.parse("04/04/2016"),
+      contract_end: Date.parse("05/04/2018"),
+      contract_status: 0,
+      contract_unspsc: 72000000,
+      contract_details: "Royal Victorian Eye and Ear Hospital Redevelopment (RVEEH)  Main Works Basement and Civil Works",
+      vt_contract_index: "HYCR037"
+    }
+    assert_equal expected_response, contract_object
+  end
+
+  test "extract contract fails ..." do
+    contract_object = extract_contract_data("", 0) # text, contract_index
+    expected_response = {
+      gov_entity: "",
+      gov_entity_contract_numb: "",
+      gov_entity_id_numb: 0,
+      contract_title: "",
+      contract_type: 0,
+      contract_value: 0,
+      value_type: 0,
+      contract_start: Date.parse("11/10/1900"),
+      contract_end: Date.parse("11/10/1900"),
+      contract_status: 0,
+      contract_unspsc: 0,
+      contract_details: "",
+      vt_contract_index: ""
+    }
+    assert_equal expected_response, contract_object
+  end
+
+
 end
