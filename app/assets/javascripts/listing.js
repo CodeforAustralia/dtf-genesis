968b6bb8bcdadf2fa645a5f8b8b2e7abf729d2ef
Elmer Ibayan <eibayan@gmail.com>
2016-11-07 16:01:36 +1100

    Cleanup files

--- app/assets/javascripts/listing.js
+++
@@ -1,7 +0,0 @@
-window.findContractId = function() {
-$('.contracts-row').on('click', function(){
-    idClick = $(this).attr("data-id");
-    // window.open( "http://localhost:3000/contracts/" + idClick, "_self"); //for development <--still need to fix this in the future
-    window.open( "http://dtf-genesis.herokuapp.com/contracts/" + idClick, "_self");
-  });
-};
