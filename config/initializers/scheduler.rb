773ebbe42cf23ce898e6d3e17f55f2814b182664
PuZZleDucK <puzzleduck@gmail.com>
2016-11-08 15:39:18 +1100

    hookup email environment variables and triggers

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -11,20 +11,24 @@ daily_scrape = scheduler.every '1d', :first_at => Time.parse("11:55:00 pm") do #
   print "\n :: Completed Scraping ::"
 end
 
+
+
+
 if Time.now > Time.parse("09:00:00 am")
   start = Time.parse("09:00:00 am") + 1.days
 else
   start = Time.parse("09:00:00 am")
 end
-notify_now = scheduler.every '1d', :first_at => start do
-# notify_now = scheduler.every '1m', :first_at => Time.now + 7 do #  DEBUG
+# notify_now = scheduler.every '1d', :first_at => start do
+notify_now = scheduler.every '1m', :first_at => Time.now + 7 do #  DEBUG
   contracts_starting_today = Contract.where(start_date: Time.now)
-  contracts_starting_today.each do |contract|
-    tweet_contract_start contract
-    email_contract_start contract "puzzleduck+dtf@gmail.com"
+  contracts_starting_today.each do |cont|
+    tweet_contract_start cont
+    email_contract_start cont, "puzzleduck+dtf@gmail.com"
   end
   contracts_ending_today = Contract.where(end_date: Time.now)
-  contracts_ending_today.each do |contract|
-    tweet_contract_end contract
+  contracts_ending_today.each do |cont|
+    tweet_contract_end cont
+    email_contract_end cont, "puzzleduck+dtf@gmail.com"
   end
 end
