c919feca7aa3c918cec5609ba048b0c4b7b84c68
Elmer Ibayan <eibayan@gmail.com>
2016-11-08 13:59:44 +1100

    Create db migration for location_code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107233917) do
+ActiveRecord::Schema.define(version: 20161107234433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,11 @@ ActiveRecord::Schema.define(version: 20161107233917) do
     t.integer  "vt_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "location_codes", force: :cascade do |t|
+    t.string "code"
+    t.string "description"
   end
 
   create_table "locations", force: :cascade do |t|
