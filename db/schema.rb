6f84b72754b9180ad9d43debf5f726cbf7eb8fe9
Elmer Ibayan <eibayan@gmail.com>
2016-11-14 15:07:17 +1100

    Update schema test db

--- db/schema.rb
+++ db/schema.rb
@@ -188,23 +188,6 @@ ActiveRecord::Schema.define(version: 20161114035702) do
     t.string   "parent_category"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  end
-
   add_foreign_key "cpr_scores", "cpr_reports"
   add_foreign_key "projects", "departments"
 end
