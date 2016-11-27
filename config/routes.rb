8f56ade2f666dde901773fdec2637d0528caaa61
PuZZleDucK <puzzleduck@gmail.com>
2016-11-28 10:33:17 +1100

    add ssl verification route and code

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+
+  get '/.well-known/acme-challenge/:id' => 'pages#letsencrypt'
+
   # devise_for :admin_users, ActiveAdmin::Devise.config
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
   resources :csr_performance_reports
   resources :cpr_performance_reports
-  
+
   get '/contracts/search', to: 'contracts#search'
   get '/contracts/save', to: 'contracts#save'
 
