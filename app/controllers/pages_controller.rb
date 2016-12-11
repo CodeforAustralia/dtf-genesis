a8ca156d49935aa734d209fa7a95156822ce9e8d
PuZZleDucK <puzzleduck@gmail.com>
2016-12-12 09:24:47 +1100

    add prototype for departmental breakdown

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,7 @@ class PagesController < ApplicationController
     @contracts_in_365 = Contract.where("vt_start_date >= :start_date AND vt_start_date <= :end_date", {start_date: year_ago, end_date: now}).count
     @value_in_30 = sum_contract_values(Contract.where("vt_start_date >= :start_date AND vt_start_date <= :end_date", {start_date: month_ago, end_date: now}))
     @value_in_365 = sum_contract_values(Contract.where("vt_start_date >= :start_date AND vt_start_date <= :end_date", {start_date: year_ago, end_date: now}))
+    @department_breakdown = sum_contract_values_by_department(Contract.where("vt_start_date >= :start_date AND vt_start_date <= :end_date", {start_date: month_ago, end_date: now}))
   end
 
   def about
@@ -15,6 +16,10 @@ class PagesController < ApplicationController
 
   def letsencrypt
     render text: "iiyNgEQOuqL_K7XTnHg3vsG2fC5tNy-Ggaa2MlyRjaI.KPrMSemCEGMLoDSXAxxnq3xTinyladwIZkHNg7y2tM8"
+  end
+
+  def sum_contract_values_by_department(contracts)
+    return [{name: "test", value: 0},{name: "test2", value: 2}]
   end
 
   def sum_contract_values (contracts)
