5aec971defc977fd29fff1e69616179807f35169
PuZZleDucK <puzzleduck@gmail.com>
2017-01-03 16:54:08 +1100

    editable fields in confirmation reporting form

--- app/helpers/suppliers_helper.rb
+++ app/helpers/suppliers_helper.rb
@@ -1,2 +1,15 @@
 module SuppliersHelper
+
+
+  def lookup_supplier_name supplier_id
+    supplier = Supplier.where(id: supplier_id).first
+    if supplier
+      supplier.name
+    else
+      ""
+    end
+  end
+
+
+
 end
